67fcca11e36d52e35f33126ed27807507370ee8c
Insanitus <22409665+insanitus@users.noreply.github.com>
2018-09-06 21:31:05 -0400

    Uso de lineas comentadas y operacion matematica suma

---
+++ ej3.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+ //Esta es una simple linea para comentar el código.   
+ 
+ #Esta también es una simple lina para comentar el código.
+
+/*
+ Estas son multiples lineas de comentarios
+ que estan en diversos
+ parrafos. 
+*/
+
+//Tambien puedes usar comentarios para dejar fuera parte de la linea de código
+$x=5 /* + 15 */ + 5;
+echo $x;
+
+?>
+
+
+</html>
+</body>
