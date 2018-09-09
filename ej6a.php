26b519f337eb24dbfa7d61f4816557b69acdc776
Insanitus <22409665+insanitus@users.noreply.github.com>
2018-09-09 01:16:26 -0400

    Operaciones matematica suma y resta con las variables

---
+++ ej6a.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<?php
+
+ $txt= "  Aloha Mundo !";
+ $x=5;
+ $y=10.5;
+ 
+ //Mostando el contenido de la variable txt 
+ Echo $txt;
+ echo "<br>";
+
+ //Mostrando el contenido de la variable x
+ echo $x;
+ echo "<br>";
+
+ #Mostrando el contenido de la variable y
+ echo $y;
+ echo "<br>";
+
+ // Realizando operaciones matematicas con 
+ // las variables numericas
+
+ echo "Suma";
+ echo $x + $y;
+ echo <"br">;
+ echo <"br">;
+
+ echo "Resta";
+ echo $x - $y;
+ echo <"br">;
+ echo <"br">; 
+ 
+ 
+
+?>
+
+
+
+</body>
+</html>
