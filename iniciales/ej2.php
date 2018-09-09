06ef66ce2704f1a60876570731ff54f1b914870f
Insanitus <22409665+insanitus@users.noreply.github.com>
2018-09-09 01:20:12 -0400

    Carpeta de ejercicios iniciales

---
+++ iniciales/ej2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1> Mi primera pagina en PHP</h1>
+
+<?php
+ echo "¡Aloha Mundo!";
+?>
+
+</body>
+</html>
---
+++ iniciales/ej4.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+//   Esto es para demostrar que
+//   la palabras clave
+//   NO son sensibles a como 
+//   se escriban
+
+<br>
+<br>
+
+<?php
+ECHO " Aloha Mundo !<br>";
+echo " Aloha Mundo !<br>";
+EcHO " Aloha Mundo !<br>";
+?>
+
+
+</html>
+</body>
