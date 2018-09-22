b5461d6f20ffa4f216f572949e5cdd87e6f3a1f8
Insanitus <22409665+insanitus@users.noreply.github.com>
2018-09-22 18:43:56 -0400

    Formas distintas de utiliar una variable

---
+++ EjIniciales/ej8.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+
+
+<?php
+ 
+//Forma Distinta de utilizar
+//una variable
+
+ $txt="LoremIpsum.com";
+ echo "Consulta la pagina ". $txt . " !";
+?>
+
+
+</body>
+</html>
