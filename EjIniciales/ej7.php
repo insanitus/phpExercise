96381a4b1ef6a38171bbbbf09520b1b7665bb1db
Insanitus <22409665+insanitus@users.noreply.github.com>
2018-09-22 18:38:17 -0400

    Utilizando echo para mostrar mensajes

---
+++ EjIniciales/ej7.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+
+<?php
+
+/*
+  En PHP  echo  es muy usado para
+  imprimir el valor de las variables
+  en el navegador
+
+*/
+
+
+  $txt="LoremIpsum.com" ;
+  echo  "<br>"
+  echo  "Consulta la pagina  $txt"
+
+?> 
+
+
+</body>
+</html>
