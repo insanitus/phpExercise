9af74c653d4e4663a6755e833bdeffcd9d187de8
Insanitus <22409665+insanitus@users.noreply.github.com>
2018-09-22 18:49:05 -0400

    Utilizando las variables en operaciones de suma, resta, multiplicacion

---
+++ EjIniciales/ej9a.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+El valor de x es 5
+<br>
+El valor de y es 4
+<br>
+<br>
+<br>
+<?php
+
+//Uso de las variables
+//de forma matematica
+
+ $x=5;
+ $y=4;
+ 
+ echo "La sumatoria de x + y es igual a ";
+
+ echo $x + $y;
+ echo "<br>";
+ echo " --- ";
+ echo "<br>";
+ 
+ echo "La resta de x - y es igual a ";
+ echo $x - $y;
+ echo "<br>";
+ echo " --- ";
+ echo "<br>";
+
+ echo "La multiplicacion de x * y es igual a ";
+ echo $x*$y; 
+ echo "<br>";
+ echo " --- ";
+ echo "<br>";
+
+ echo "La sumatoria de tres veces x es igual a ";
+ echo $x + $x + $x;
+ echo "<br>";
+ echo " --- ";	
+ echo "<br>";
+
+ echo "La sumatoria de tres veces y es igual a ";
+ echo $y + $y + $y;
+ echo "<br>";
+ echo " --- ";
+ echo "<br>";	
+?>
+
+
+
+</body>
+</html>
