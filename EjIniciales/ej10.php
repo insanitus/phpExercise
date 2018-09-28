a64c0677c81ded097a47e03552b5facce708357d
Insanitus <22409665+insanitus@users.noreply.github.com>
2018-09-27 21:56:19 -0400

    Variable con alcance local, se corrigio comillas

--- EjIniciales/ej10.php
+++ EjIniciales/ej10.php
@@ -4,26 +4,16 @@
 
 
 <?php
-
- /* Una variable declarada fuera
-    de la funcion 
-    tiene un acceso general
-    y solo puede ser accesible
-    fuera de la funcion
-*/
-
-
  $x = 5; // alcance global
 
  function miPrueba() {
    //Utilizando x dentro de esta función generara un error
-  echo "<p> Variable x dentro de la funcion es: $x </p>; 
+  echo "<p> Variable x dentro de la funcion es: $x </p>"; 
  }
 
  miPrueba();
 
  echo "<p> Variable x fuera de la función es: $x </p>";
-
 
 
 ?>
