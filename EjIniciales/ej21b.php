ca8574179bda79acf73f47cbe12791a36f9b40e2
Insanitus <22409665+insanitus@users.noreply.github.com>
2018-10-10 01:16:19 -0400

    Decimales a ser mostrados

---
+++ EjIniciales/ej21b.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<?php
+#Declaracion de flotantes
+$a = 3.1;
+$b = 3.14;
+$c = 3.141;
+$d = 3.1415;
+
+
+#Mostrando el resultado en el navegador
+#Probando la cantidad de decimales que php puede mostrar
+
+var_dump($a);
+echo "<br>";
+
+var_dump($b);
+echo "<br>";
+
+var_dump($c);
+echo "<br>";
+
+
+var_dump($d);
+
+
+
+
+
+?>
+
+
+</body>
+</html>
