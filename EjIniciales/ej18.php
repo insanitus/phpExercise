d179d28d8c8c27c3dedfd33514896a40f1eff029
Insanitus <22409665+insanitus@users.noreply.github.com>
2018-10-10 00:45:29 -0400

    Uso de print con variables dispersas

---
+++ EjIniciales/ej18.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<?php
+#Declarando las variables de texto
+$txt1 = "Códeando en PHP";
+$txt2 = "LoremIpsum.com";
+
+#Declarando las variables numericas
+$x = 15;
+$y = 3;
+
+#Mostrandolas en pantalla
+
+print "<h2>" . $txt1 . "</h2>";
+print "y mostrandolo en  " . $txt2 . "<br>";
+print $x + $y;
+
+
+?>
+
+
+</body>
+<html>
