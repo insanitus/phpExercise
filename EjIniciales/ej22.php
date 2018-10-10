3b546b683e2dcd9fa1b9b7b9379e5f9437f438bf
Insanitus <22409665+insanitus@users.noreply.github.com>
2018-10-10 02:08:06 -0400

    Arreglos en php

---
+++ EjIniciales/ej22.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<?php
+#Declarando la variable
+$cars = array("Volvo", "BMW", "Toyota");
+
+
+#Mostrando en el navegador su contenido
+var_dump($cars);
+
+
+
+?>
+
+
+</body>
+</html>
