b0f53771e34101f3cb6cb4423525106410a40b65
Insanitus <22409665+insanitus@users.noreply.github.com>
2018-10-10 00:57:37 -0400

    Cadenas en php

---
+++ EjIniciales/ej19.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<?php
+#Declarando las variables
+$x = " Aloha World ";
+$y = " Bonjour Monde  ";
+
+
+#Mostrandolas en el navegador
+
+echo $x;
+echo "<br>";
+echo $y;
+
+
+?>
+
+
+</body>
+<html>
