9059917bf0c6132657627dadb6184a973bd4f02d
Insanitus <22409665+insanitus@users.noreply.github.com>
2018-10-10 00:24:25 -0400

    Uso de echo con varibles dispersas

---
+++ EjIniciales/ej16.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$txt1 = "Aprendiendo PHP";
+$txt2 =  "LoremIpsum.com";
+$x = 6;
+$y = 4;
+
+echo "<h2>" . $txt1 . "</h2>";
+echo  "Revisa la pagina en  " . $txt2 . "<br>";
+echo $x + $y;
+
+
+?>
+
+</body>
+</html>
