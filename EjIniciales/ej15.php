13df7e630d023a3c68056a3ff3a9e9ca4a37f376
Insanitus <22409665+insanitus@users.noreply.github.com>
2018-10-10 00:15:59 -0400

    Uso variable de echo

---
+++ EjIniciales/ej15.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "<h2>Codeando en PHP </h2>";
+echo "Aloha Web <br>";
+echo "Aprendiendo sobre PHP<br>";
+echo "Esta "," cadena "," de "," caracteres "," esta "," compuesta "," con multiples frases<br> ";
+
+
+?>
+
+
+</body>
+</html>
