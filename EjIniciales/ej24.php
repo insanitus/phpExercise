20969df4aab2ac0165e3ff7bc6b568d383e8198f
Insanitus <22409665+insanitus@users.noreply.github.com>
2018-10-10 02:22:50 -0400

    Valor Nulo

---
+++ EjIniciales/ej24.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<?php
+
+#Declarando las variables
+$x = "Bonjour Monde";
+$x = null;
+
+#Mostrandolas en el navegador
+var_dump($x);
+
+
+
+?>
+
+
+</body>
+</html>
