87abfc2253f6ba73ebb45a56ab6cd068c8c0d961
Insanitus <22409665+insanitus@users.noreply.github.com>
2018-10-12 23:45:01 -0400

    Utilizando un anagrama

---
+++ EjIniciales/ej27b.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<?php
+#strrev funciona para invertir una frase
+echo ("La frase; <i> 'Anita lava la tina'</i> ");
+echo ("Invertida la frase se lee <br>");
+echo strrev ("Anita lava la tina");
+
+
+
+?>
+
+
+</body>
+</html>
